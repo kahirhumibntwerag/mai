581d505e79df601cb4a3c6ac2c778d807df343da
MoMO <mhesham253@gmail.com>
2026-03-09 03:04:42 +0100

    feat: initial commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,15 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs));
-}
-
-export function formatPrice(price: number): string {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(price);
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
